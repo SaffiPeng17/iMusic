e08bc9ffa2fdda3e94e9101fc460a02dc34a766b
Saffi <saffi@rooit.me>
2022-02-12 04:05:19 +0800

    add: project & gitMessage

---
+++ iMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iMusic
+//
+//  Created by Saffi on 2022/2/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
